7c469f26e433d1b616e178ca1f4118667d3fe3b5
1nyeong <inyeong0726@gmail.com>
2023-04-05 02:11:31 +0900

    회원 서비스 테스트(중복 회원 예외 처리)

--- src/main/java/gradle/spring/service/MemberService.java
+++ src/main/java/gradle/spring/service/MemberService.java
@@ -2,14 +2,17 @@ package gradle.spring.service;
 
 import gradle.spring.domain.Member;
 import gradle.spring.repository.MemberRepository;
-import gradle.spring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
     * 회원가입
